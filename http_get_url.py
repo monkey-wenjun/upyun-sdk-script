90bc3092645ed44c6936b2bd54c1d9c0f9bb54cc
wenjun <admin@fangwenjun.com>
2016-04-20 11:10:08 +0800

    Update http_get_url.py

--- http_get_url.py
+++ http_get_url.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 import requests
+import time
 
 def http_get_url():
-    srcpath = "" 
-    dstpath = "."
-    serverName = ""
-    separator = ""
-    secureName = ""
-    file = open(srcpath)
+    srcpath = "D:\suntimereport_file.txt" 
+    dstpath = "D:\save.txt"
+    serverName = "suntimereport"
     file_str = open(dstpath, 'wb')
-    for url_path in file.readlines():
-        url = "http://"+serverName+".b0.upaiyun.com"+url_path.strip('\n')
-        req_url = requests.head(url)
-        print url
-        if req_url.status_code == 404:
-           se_url = url+separator+secureName
-           req_se_url = requests.head(se_url)
-           if req_se_url.status_code == 200:
-           		print se_url
-           		file_str.write(url)
+    count = 0
+    with open(srcpath,'r') as f:
+	    for url_path in f:
+	        starttime =  time.time()
+	        try:
+	        	url = "http://"+serverName+".b0.upaiyun.com"+url_path.strip('\n')
+	        	req_url = requests.head(url,timeout=5)
+	        except Exception,e:
+	        	print e
+	        endtime =  time.time()
+	        count +=1
+	        times = round(endtime-starttime,2)
+		restatus = req_url.status_code
+	        print count,url,times		
+		print restatus
+	    if restatus == 404:
+	        file_str.write(url)
 
     file.close()
     file_str.close()
